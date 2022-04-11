169701d3ceadd50f7d42e6eb3792401e0e01f0b5
rbm717 <47369758+rbm717@users.noreply.github.com>
2022-04-11 12:27:59 -0700

    Add files via upload

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,23 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+      super(scene, x, y, texture, frame);
+  
+      // add object to existing scene
+      scene.add.existing(this);
+      this.points = pointValue;
+      this.moveSpeed = game.settings.spaceshipSpeed;
+    }
+
+    update(){
+        //move spaceship left
+        this.x -= this.moveSpeed;
+        //wraps spaceship back around to right side
+        if(this.x <= 0 - this.width){
+            this.x = game.config.width;
+        }
+    }
+
+    reset(){
+        this.x = game.config.width;
+    }
+}
